8b1df05fb760b08f7010985793e61cdb76c0fd4b
Aswinne <aswincasis@gmail.com>
2023-10-15 20:19:32 +0800

    day006

---
+++ day004.java
@@ -0,0 +1,7 @@
+public class day004 {
+    public static void main(String[] args) {
+        //boolean
+        boolean a = 5>4;
+        System.out.println(a);
+    }
+}
---
+++ day005.java
@@ -0,0 +1,7 @@
+public class day005 {
+
+    public static void main(String[] args) {
+        char jenisKelamin = 'L';
+        System.out.println(jenisKelamin);
+    }
+}
