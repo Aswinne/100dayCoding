53223f006c9d9b7299dbbc982f7707aba983232b
Aswinne <aswincasis@gmail.com>
2023-10-20 19:20:57 +0800

    day011

---
+++ day010.java
@@ -0,0 +1,7 @@
+public class day010 {
+    public static void main(String[] args) {
+        short tinggiBadan = 170;
+        double beratBadan = 65;
+        System.out.println(tinggiBadan/beratBadan);
+    }
+}
