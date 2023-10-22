0c7e4197acce105cb0deac1f67cacc502bcb908b
Aswinne <aswincasis@gmail.com>
2023-10-22 18:47:13 +0800

    day13

---
+++ day011.java
@@ -0,0 +1,8 @@
+public class day011 {
+    public static void main(String[] args) {
+        int umur1 = 100;
+        int umur2 = 200;
+        int umur3 = 100;
+        System.out.println(umur1-umur2+umur3);
+    }
+}
---
+++ day012.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class day012 {
+
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("masukkan umur :");
+        
+        int umur=input.nextInt();
+        System.out.println("umur anda :"+umur);
+    }
+}
