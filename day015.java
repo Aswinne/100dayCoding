166f0bf552968e5e9f9eafeee867fb02994aa0b5
Aswinne <aswincasis@gmail.com>
2023-10-25 23:35:25 +0800

    day016

---
+++ day015.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class day015 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("masukkan angka yang di inginkan :");
+        
+        int nilai = input.nextInt();
+        
+        for (int i = 0; i < nilai; i++) {
+            System.out.println(1-i);
+}
+    }}
