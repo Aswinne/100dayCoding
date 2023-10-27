030dff1228d81c5c807498f048a382182b5533e8
Aswinne <aswincasis@gmail.com>
2023-10-27 20:56:01 +0800

    day018

---
+++ day017.java
@@ -0,0 +1,10 @@
+public class day017 {
+    public static void main(String[] args) {
+        String[] daftarNama={"aswin","budi"};
+        
+        for (String nama : daftarNama){
+            System.out.println(nama);
+        }
+    }
+}
+
