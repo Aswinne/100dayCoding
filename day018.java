73fd7c4246bfaf4ce625fef1b94b2e8a7fb51517
Aswinne <aswincasis@gmail.com>
2023-10-28 14:04:08 +0800

    day019

---
+++ day018.java
@@ -0,0 +1,9 @@
+public class day018 {
+    public static void main(String[] args) {
+        String[] buahan={"kelapa","apel","mangga","pisang"};
+        
+        for (String buah : buahan){
+            System.out.println(buah);
+        }
+    }
+}
