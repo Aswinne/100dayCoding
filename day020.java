da7463044149cef0c7b081efd90d53094e57cdc5
Aswinne <aswincasis@gmail.com>
2023-10-30 21:52:48 +0800

    day021

---
+++ day020.java
@@ -0,0 +1,11 @@
+public class day020 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 8) {
+                System.out.println("aswin");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
