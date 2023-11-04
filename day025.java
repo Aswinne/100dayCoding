0d0f3a911ea4cec84a53d7a639187a1f125900c4
Aswinne <aswincasis@gmail.com>
2023-11-05 06:41:59 +0800

    day026

---
+++ day025.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class day025 {
+    public static void main(String[] args) {
+        ArrayList<String> namaList = new ArrayList<String>();
+
+        // Menambahkan aswin
+        namaList.add("Aswin");
+
+        // Menampilkan ArrayList sebelum perubahan
+        System.out.println("ArrayList sebelum perubahan:");
+        System.out.println(namaList);
+        
+    }
+}
+
