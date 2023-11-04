5ddb995364a9e61b00f0a12857ad7e20be4d0e23
Aswinne <aswincasis@gmail.com>
2023-11-04 20:04:35 +0800

    day025

--- day023.java
+++ day023.java
@@ -2,16 +2,10 @@ import java.util.ArrayList;
 
 public class day023 {
     public static void main(String[] args) {
-        // Membuat ArrayList untuk menyimpan nama
+ 
         ArrayList<String> namaList = new ArrayList<>();
-
-        // Menambahkan nama "Aswin" ke ArrayList
         namaList.add("Aswin");
-
-        // Menambahkan nama "Fadli" ke ArrayList
         namaList.add("Fadli");
-
-        // Menampilkan isi dari ArrayList
         System.out.println("Isi dari ArrayList:");
         for (String nama : namaList) {
             System.out.println(nama);
---
+++ day024.java
@@ -0,0 +1,11 @@
+public class day024 {
+    public static void main(String[] args) {
+        String nama1 = "aswin";
+        String nama2 = " salam";
+                
+        String nama3 = nama1 + nama2;
+                
+        System.out.println(nama3);
+    }
+}
+        
