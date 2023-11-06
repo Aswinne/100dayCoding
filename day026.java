1a2f77a17247751be0318ff00fecc1ad0aca44e7
Aswinne <aswincasis@gmail.com>
2023-11-06 13:58:42 +0800

    day027

---
+++ day026.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class day026{
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        System.out.println("masukkan data siapa :");
+        String nama = data.nextLine();
+        System.out.println("masukkan alamat :");
+        String alamat = data.nextLine();
+        System.out.println("masukkan umur :");
+        int umur = data.nextInt();
+        System.out.println("masukkan tinggi :");
+        short tinggi =data.nextShort();
+        
+
+    }
+}
