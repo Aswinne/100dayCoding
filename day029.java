afe06536f5b8b9254b855b95f7a3833f59f92515
Aswinne <aswincasis@gmail.com>
2023-11-09 20:04:12 +0800

    day030

---
+++ day029.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class day029 {
+    public static void main(String[] args) {
+        Scanner aswin = new Scanner(System.in);
+        System.out.println("masukkan nama yang ingin di tampilkan :");
+
+        String nama = aswin.nextLine();
+        System.out.println(nama+" adalah seorang pahlawan kegelapan "+nama+" juga bisa merebut cintamu wkwk...");
+    }
+}
