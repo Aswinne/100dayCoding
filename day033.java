1eb4246657ce8ac25f76bbe278ffbaeeae0ee720
Aswinne <aswincasis@gmail.com>
2023-11-13 00:47:49 +0800

    day034

---
+++ day033.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class day033 {
+    public static void main(String[] args) {
+        Stack<String>mely = new Stack <> ();
+        
+        mely.push ("aswin");
+        mely.push ("fahri");
+        mely.push ("fadli");
+        
+        System.out.println(mely.peek());
+        System.out.println(mely.peek());
+        System.out.println(mely.peek());
+    }
+    
+}
