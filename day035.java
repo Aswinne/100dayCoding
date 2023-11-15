0ba89d8e7d058af832c04f6a1cf45817c54a2b11
Aswinne <aswincasis@gmail.com>
2023-11-15 14:37:47 +0800

    day036

---
+++ day035.java
@@ -0,0 +1,12 @@
+public class day035 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(); // Pindah ke baris baru setelah mencetak bintang pada satu baris
+        
+        }
+    }
+    
+}
