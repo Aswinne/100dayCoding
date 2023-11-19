a1defaa929a4350ea3cb4fc8cea66d1cad5ce352
Aswinne <aswincasis@gmail.com>
2023-11-19 20:49:56 +0800

    day040

---
+++ day039.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class day039 {
+    public static void main(String[] args) {
+        Scanner aswin = new Scanner(System.in);
+        System.out.print("masukkan nilai :");
+        int nilai = aswin.nextInt();
+        
+        for (int i = 0; i <= nilai; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            
+                
+                
+            }
+            System.out.println();
+            
+        }
+    }
+}
