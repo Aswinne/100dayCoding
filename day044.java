8f33c632cc7d1958a05eddc3b8dfb2477db76468
Aswinne <aswincasis@gmail.com>
2023-11-24 06:47:05 +0800

    day045

---
+++ day044.java
@@ -0,0 +1,10 @@
+public class day044 {
+    public static void main(String[] args) {
+        String [] nilai = {"aswin","asri","pahri"};
+        for (String nilaiString : nilai) {
+            System.out.println(nilaiString);
+            
+        }
+                
+    }
+}
