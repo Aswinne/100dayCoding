7cd06dc8cd98ef2f97704550bf24b6c0807eb8f1
Aswinne <aswincasis@gmail.com>
2023-11-27 19:03:47 +0800

    day048

---
+++ day047.java
@@ -0,0 +1,12 @@
+public class day047 {
+    {
+        public static void main(String[] args) {
+            String[] buah={"mangga","apel"};
+            
+            for (int i = 0; i < buah.length; i++) {
+                System.out.println("daftar buah buahan :"+buah[i]);
+                
+            }
+        }
+    }
+}
