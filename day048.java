dc05fb5bf5056fa226d069acb7568850ce6b2dfe
Aswinne <aswincasis@gmail.com>
2023-11-28 11:41:19 +0800

    day049

---
+++ day048.java
@@ -0,0 +1,8 @@
+public class day048 {
+    public static void main(String[] args) {
+        int [] data ={70,90,86,95,78};
+        
+       int jumlahData=data[0]+data[1]+data[2]+data[3]+data[4];
+       System.out.println("jumlah data mahasiswa :"+jumlahData);
+    }
+}
