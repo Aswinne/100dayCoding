45a4b54edb00bcfbbbd657edcd9e82f799c75c75
Aswinne <aswincasis@gmail.com>
2023-11-29 19:45:41 +0800

    day050

---
+++ day049.java
@@ -0,0 +1,14 @@
+
+public class day049 {
+    public static void main(String[] args) {
+        int data []={45,78,71,89,93,58,62,63,34,78};
+        
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > 70){
+                System.out.println("lulus :"+data[i]);
+            
+            }
+        }
+    }
+}
+
