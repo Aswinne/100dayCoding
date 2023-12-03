596f18d7fa7f1da707d2d6c00a1cdee7e176144e
Aswinne <aswincasis@gmail.com>
2023-12-03 20:31:01 +0800

    day054

---
+++ day053.java
@@ -0,0 +1,14 @@
+public class day053 {
+    public static void main(String[] args) {
+        String nama [] = {"aswin","budi","dedi","nana"};
+        //while
+        int a = 0;
+        do{
+            System.out.println(nama[a]);
+            ++a;
+        }while (a<nama.length);
+            
+            
+        }
+
+    }
