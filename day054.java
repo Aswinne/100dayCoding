da2f829d0686a0d963f313e0320ef7997f17edaf
Aswinne <aswincasis@gmail.com>
2023-12-04 05:24:29 +0800

    day055

---
+++ day054.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class day054 {
+    public static void main(String[] args) {
+        String data []= {"wiwi","wawa","wewe","wowo"};
+        System.out.println(Arrays.toString(data));
+    }
+}
