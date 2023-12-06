7ceee8c898176ad2a74e6d549b739204f6f30c3f
Aswinne <aswincasis@gmail.com>
2023-12-06 19:04:10 +0800

    day057

---
+++ day056.java
@@ -0,0 +1,13 @@
+public class day056 {
+    public static void main(String[] args) {
+        int data[][]={
+            {2,3,4},
+            {5,6,7},
+            {8,9,1}
+        };
+        for(int i = 0; i < data.length; i++) {
+            for(int j = 0; j < data[i].length; j++) {
+                System.out.print(data[i][j]);
+        }
+    }
+}}
