04db23654608b9f87100483391f969675b52b629
Aswinne <aswincasis@gmail.com>
2023-12-08 22:28:38 +0800

    day059

---
+++ day058.java
@@ -0,0 +1,8 @@
+
+public class day058 {
+    public static void main(String[] args) {
+        int nomber [][]={{1,2,3},{6,7,8}};
+        nomber[1][2]=10;
+        System.out.println(nomber[1][2]);
+    }
+}
