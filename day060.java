1d1a229ff8c509b6563971e65113e559a47925ae
Aswinne <aswincasis@gmail.com>
2023-12-10 06:36:40 +0800

    day061

---
+++ day060.java
@@ -0,0 +1,13 @@
+
+public class day060 {
+    public static void main(String[] args) {
+        String [][] nama= {{"aswin","08111"},{"awwing","08222"},
+            {"akuu dan kamu","088888"}};
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println("nama :"+nama[i][0]);
+            System.out.println("nomor :"+nama[i][1]);
+            System.out.println("---------------------");
+            
+        }
+    }
+}
