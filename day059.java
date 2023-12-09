a5dfda79e6421532b998402876aa6d6522b9295e
Aswinne <aswincasis@gmail.com>
2023-12-09 12:44:35 +0800

    day060

---
+++ day059.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class day059 {
+    public static void main(String[] args) {
+        String keluarga[]={"aswin","aswat","asraf"};
+        System.out.println(Arrays.toString(keluarga));
+    }
+}
