b7beae8b0f5da6a268adf39f887642976fce70de
Aswinne <aswincasis@gmail.com>
2023-12-13 13:37:22 +0800

    day064

---
+++ day063.java
@@ -0,0 +1,15 @@
+public class day063
+{
+   public static void main (String args[])
+   {
+    //Deklarasi array
+    String[][]huruf = {
+        {"A","B","C"},
+        {"D","E","F"},
+        {"G","H","I"} 
+    };
+    //Menampilkan isi array pada indeks ke-[0][1] dan [2][0]
+    System.out.println(huruf[0][1]);
+    System.out.println(huruf[2][0]);
+    }
+ }
