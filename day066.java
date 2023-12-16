086cd988598d57dfb1e4e666316bd41ec58e1e27
Aswinne <aswincasis@gmail.com>
2023-12-16 22:06:49 +0800

    day067

---
+++ day066.java
@@ -0,0 +1,8 @@
+public class day066 {
+    public static void main(String[] args) {
+        double data[]={2.1,4.1,0.9,0.0};
+        for (double i = 0; i < data.length; i++) {
+            System.out.println("data");
+        }
+    }
+}
