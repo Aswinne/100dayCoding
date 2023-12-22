84594dd78755a0a1f66d4e15f5eefdc6cc55fc2f
Aswinne <aswincasis@gmail.com>
2023-12-23 06:54:15 +0800

    day074

---
+++ day073.java
@@ -0,0 +1,9 @@
+public class day073 {
+    public static void main(String[] args) {
+        int a = 0;
+        while(a <= 10){
+            System.out.println("nilai ke-"+a);
+            a++;
+        }
+    }
+}
