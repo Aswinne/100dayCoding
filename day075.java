7bb64f8938ac99bb6e92e84230e0e3f77f008da1
Aswinne <aswincasis@gmail.com>
2023-12-25 14:38:46 +0800

    day076

---
+++ day075.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class day075 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("masukkan batas atas angka :");
+        int angka = sc.nextInt();
+        System.out.println("jumlah angka :"+angka / 3);
+    }
+}
+
