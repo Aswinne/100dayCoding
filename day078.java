4ca8a3eb670848dd80f2296b3b67cb68def5468c
Aswinne <aswincasis@gmail.com>
2023-12-28 13:05:57 +0800

    day079

---
+++ day078.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class day078{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukkan nama :");
+        String a = sc.nextLine();
+        a = "riswan";
+        System.out.println("nama anda ="+a);
+    }
+}
+
