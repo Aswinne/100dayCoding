59d29fb736f3f2a81d07e1f545f10db06ba623f4
Aswinne <aswincasis@gmail.com>
2023-12-29 02:15:38 +0800

    day080

---
+++ day079.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class day079 {
+    public static void main(String[] args) {
+       Scanner input=new Scanner(System.in); 
+        System.out.print("masukkan panjang : ");
+        int panjang=input.nextInt();
+        
+        System.out.print("masukkan lebar : ");
+        int lebar=input.nextInt();
+        
+        int[][] angka=new int [panjang][lebar];
+
+        for (int i = 0; i < panjang; i++) {
+            for (int j = 0; j < lebar; j++) {
+                System.out.print("masukkan data["+i +"]["+j +"] = ");
+                angka [i][j] = input.nextInt();
+                
+            }
+            System.out.println();
+            
+            
+            
+        }
+        for (int i = 0; i < panjang; i++) {
+            for (int j = 0; j < lebar; j++) {
+                System.out.print(angka[i][j]);
+                
+            }
+            System.out.println();
+        }
+        input.close();
+       
+                
+            }
+            
+        }
+    
+   
+
