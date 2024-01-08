e2ebd3a927be1a3b744ad9d4366188f0c0b21a0a
Aswinne <aswincasis@gmail.com>
2024-01-08 21:44:57 +0800

    day091

---
+++ day090.java
@@ -0,0 +1,16 @@
+public class day090 {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = i * 3 + j + 1;
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
